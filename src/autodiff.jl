1fdcfab85805495f3b0dc45b91e59080f8da675b
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-12-29 00:57:24 +0100

    add chunk_size to autodiff

--- src/autodiff.jl
+++ src/autodiff.jl
@@ -2,9 +2,11 @@
 # assuming that f takes a Vector{T} of length n
 # and writes the result to a Vector{T} of length m
 
+# TODO: Update chunk_size to ~10 when https://github.com/JuliaDiff/ForwardDiff.jl/issues/36
+# is resolved
 function autodiff{T <: Real}(f!,::Type{T}, m)
     permf!(yp, xp) = f!(xp, yp)
-    permg! = jacobian(permf!, mutates = true, output_length = m)
+    permg! = jacobian(permf!, mutates = true, output_length = m, chunk_size = 1)
     g!(x, fx) = permg!(fx, x)
 
     return DifferentiableMultivariateFunction(f!, g!)
