99552fef9ffe87abe2e16deb41d8da4ca848f1d3
matthieugomez <gomez.matthieu@gmail.com>
2016-01-29 12:44:18 -0500

    update test

--- test/sparse.jl
+++ test/sparse.jl
@@ -39,7 +39,7 @@ r = mcpsolve(df, [-Inf;-Inf], [Inf; Inf], [-0.5; 1.4], reformulation = :minmax)
 @test norm(r.zero - [ 0; 1]) < 1e-8
 
 # Test given sparse
-df = DifferentiableGivenSparseMultivariateFunction(f!, g!, spzeros(2, 2))
+df = DifferentiableGivenSparseMultivariateFunction(f_sparse!, g_sparse!, spzeros(2, 2))
 r = nlsolve(df, [ -0.5; 1.4], method = :trust_region, autoscale = true)
 @test converged(r)
 @test norm(r.zero - [ 0; 1]) < 1e-8
