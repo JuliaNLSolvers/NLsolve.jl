5abab8a7aa0de8ae92baf530136ce2b2c3527df2
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-01-29 18:18:44 +0100

    fix unique function names

--- test/difficult_mcp.jl
+++ test/difficult_mcp.jl
@@ -3,15 +3,15 @@
 # Presented in Miranda and Fackler (2002): "Applied Computational Economics and
 # Finance", p. 51
 @testset "difficult_mcp" begin
-function f!(x, fvec)
+function f_diffmcp!(x, fvec)
     fvec[1] = (1-x[1])^2-1.01
 end
 
-function g!(x, fjac)
+function g_diffmcp!(x, fjac)
     fjac[1] = 2(x[1]-1)
 end
 
-df = DifferentiableMultivariateFunction(f!, g!)
+df = DifferentiableMultivariateFunction(f_diffmcp!, g_diffmcp!)
 
 solution = [ 2.004987562 ]
 
