09e00287d51b6b934b3ecafbf5976bb6b6669b8e
Simon Byrne <simonbyrne@gmail.com>
2017-11-21 06:58:27 -0800

    avoid use of void tags for autodiff (#131)
    * avoid use of void tags for autodiff
    
    * bump REQUIRE

--- src/autodiff.jl
+++ src/autodiff.jl
@@ -1,17 +1,18 @@
-function autodiff(f!, initial_x::AbstractArray)
-
+function autodiff(f!, initial_x::AbstractArray,
+                  jac_cfg::ForwardDiff.JacobianConfig=ForwardDiff.JacobianConfig(f!, vec(initial_x), vec(initial_x)))
+    ForwardDiff.checktag(jac_cfg, f!, vec(initial_x))
+    
     fvec! = reshape_f(f!, initial_x)
     permf! = (fx::AbstractVector, x::AbstractVector) -> fvec!(x, fx)
 
-    fx2 = vec(copy(initial_x))
-    jac_cfg = ForwardDiff.JacobianConfig(nothing, vec(initial_x), vec(initial_x))
+    fx2 = vec(copy(initial_x))    
     function g!(x::AbstractVector, gx::AbstractMatrix)
-        ForwardDiff.jacobian!(gx, permf!, fx2, x, jac_cfg)
+        ForwardDiff.jacobian!(gx, permf!, fx2, x, jac_cfg, Val{false}())
     end
 
-    fg! = (x::AbstractVector, fx::AbstractVector, gx::AbstractMatrix) -> begin
+    function fg!(x::AbstractVector, fx::AbstractVector, gx::AbstractMatrix)
         jac_res = DiffBase.DiffResult(fx, gx)
-        ForwardDiff.jacobian!(jac_res, permf!, fx2, x, jac_cfg)
+        ForwardDiff.jacobian!(jac_res, permf!, fx2, x, jac_cfg, Val{false}())
         DiffBase.value(jac_res)
     end
 
