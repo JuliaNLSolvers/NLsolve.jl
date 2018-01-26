84afabd5dfec4c1b758b80fdd5222ed391e46c2b
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-01-26 21:36:58 +0100

    Fix MCP wrt new autodiff and inplace keywords

--- src/solvers/mcp_func_defs.jl
+++ src/solvers/mcp_func_defs.jl
@@ -36,8 +36,8 @@ function mcpsolve(df::TDF,
             linesearch = linesearch, factor = factor, autoscale = autoscale)
 end
 
-function mcpsolve{T}(f!,
-                  j!,
+function mcpsolve{T}(f,
+                  j,
                   lower::Vector,
                   upper::Vector,
                   initial_x::AbstractArray{T};
@@ -51,8 +51,14 @@ function mcpsolve{T}(f!,
                   extended_trace::Bool = false,
                   linesearch = LineSearches.BackTracking(),
                   factor::Real = one(T),
-                  autoscale::Bool = true)
-    @reformulate OnceDifferentiable(f!, j!, initial_x, initial_x)
+                  autoscale = :central,
+                  inplace = true)
+    if inplace
+        df = OnceDifferentiable(f, initial_x, initial_x)
+    else
+        df = OnceDifferentiable(not_in_place(f, j)..., initial_x, initial_x)
+    end
+    @reformulate df
     nlsolve(rf,
             initial_x, method = method, xtol = xtol, ftol = ftol,
             iterations = iterations, store_trace = store_trace,
@@ -60,7 +66,7 @@ function mcpsolve{T}(f!,
             linesearch = linesearch, factor = factor, autoscale = autoscale)
 end
 
-function mcpsolve{T}(f!,
+function mcpsolve{T}(f,
                   lower::Vector,
                   upper::Vector,
                   initial_x::AbstractArray{T};
@@ -75,10 +81,14 @@ function mcpsolve{T}(f!,
                   linesearch = LineSearches.BackTracking(),
                   factor::Real = one(T),
                   autoscale::Bool = true,
-                  autodiff::Bool = false)
-#    if !autodiff
-        df = OnceDifferentiable(f!, initial_x, initial_x)
-#    end
+                  autodiff = :central,
+                  inplace = true)
+    if inplace
+        df = OnceDifferentiable(f, initial_x, initial_x, autodiff)
+    else
+        df = OnceDifferentiable(not_in_place(f), initial_x, initial_x, autodiff)
+    end
+
     @reformulate df
     nlsolve(rf,
             initial_x, method = method, xtol = xtol, ftol = ftol,
