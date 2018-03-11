b1f8ae0b185d2edba3453e0e8f1e15caa617fe57
jasmine.yangvc@gmail.com <jasmine.yangvc@gmail.com>
2018-03-10 20:28:39 -0800

    Adding trace and tests

--- src/solvers/simple_iteration.jl
+++ src/solvers/simple_iteration.jl
@@ -1,12 +1,48 @@
-# Simple Iteration
+# simple iteration
+struct simple_iteration
+end
 
-function simple_iteration{T}(df::OnceDifferentiable, initial_x::AbstractArray{T}, xtol::Real, ftol::Real, iterations::Integer)
-  f0 = df.f(initial_x) #Jasmine: this is how you call f
+macro simple_iterationtrace(stepnorm)
+    esc(quote
+        if tracing
+            dt = Dict()
+            if extended_trace
+                dt["x"] = copy(cache.x)
+            end
+            update!(tr,
+                    it,
+                    maximum(abs, value(df)),
+                    $stepnorm,
+                    dt,
+                    store_trace,
+                    show_trace)
+        end
+    end)
+end
+
+function simple_iteration{T}(df::OnceDifferentiable,
+                             initial_x::AbstractArray{T},
+                             xtol::Real,
+                             ftol::Real,
+                             iterations::Integer,
+                             store_trace::Bool,
+                             show_trace::Bool,
+                             extended_trace::Bool)
+
+  f0 = df.f(initial_x) #how to call f
   residual = Inf
   iter = 1
   xold = initial_x
 
-  #TODO: This is assuming out of place.  Would need to check and do a special csae if inplace operations.
+  #TODO:
+  copy!(cache.x, initial_x)
+  x_converged, f_converged, converged = assess_convergence(value(df), ftol)
+
+  tr = SolverTrace()
+  tracing = store_trace || show_trace || extended_trace
+  @simple_iterationtrace convert(T, NaN)
+
+  #This is assuming out of place.  Would need to check and do a special csae if inplace operations.
 
   while residual > ftol && iter < iterations
       xnew = df.f(xold) #You call the embedded f function like this
@@ -15,5 +51,17 @@ function simple_iteration{T}(df::OnceDifferentiable, initial_x::AbstractArray{T}
       iter += 1
   end
 
-  return (xold,iter)
+  #return (xold,iter)
+  return SolverResults("Simple Iteration",xold,iter,x_converged, xtol, f_converged,ftol,first(df.f_calls))
+end
+
+function simple_iteration{T}(df::OnceDifferentiable,
+                   initial_x::AbstractArray{T},
+                   xtol::Real,
+                   ftol::Real,
+                   iterations::Integer,
+                   store_trace::Bool,
+                   show_trace::Bool,
+                   extended_trace::Bool)
+    simple_iteration_(df, initial_x, convert(T, xtol), convert(T, ftol), iterations, store_trace, show_trace, extended_trace)
 end
--- test/fixed_points.jl
+++ test/fixed_points.jl
@@ -1,6 +1,7 @@
 # Test fixed points
 @testset "fixed_points" begin
 using NLsolve, Base.Test, StaticArrays
+
 #The inplace one is not implemented, so left in the old code.
 function fixedpointOLD!(f!, x0; residualnorm = (x -> norm(x,Inf)), tol = 1E-10, maxiter=100)
     residual = Inf
@@ -16,10 +17,21 @@ function fixedpointOLD!(f!, x0; residualnorm = (x -> norm(x,Inf)), tol = 1E-10, 
     return (xold,iter)
 end
 
+
+# A matrix equation fixedpoint(f,[1.0; 0.1])
 A = [0.5 0; 0 0.2]
 b = [1.0; 2.0]
-f(x) = A * x + b  # A matrix equation
-fixedpoint(f,[1.0; 0.1])
+f(x) = A * x + b
+@test fixedpoint(f, method =:simple_iteration,[1.0; 0.1], iter=100,store_trace==true,show_trace==true,
+                 extended_trace=true)[1] ≈ [2.0;2.5]
+
+
+r = fixedpoint(f,[1.0; 0.1], method = :simple_iteration)
+@test converged(r)
+@test norm(r.zero - [ 0; 1]) < 1e-8
+
+
+#@test fixedpoint(f,[1.0; 0.1]; iterations=100)[1] ≈ [2.0;2.5]
 
 @test fixedpoint(f,[1.0; 0.1]; iterations=100)[1] ≈ [2.0;2.5]
 A = [0.5 0; 0 0.2]
@@ -32,7 +44,8 @@ fixedpointOLD!(f!,[1.0; 0.2])
 A= SMatrix{2,2}(0.5,0,0,0.2)
 b = SVector(1.0, 2.0)
 f(x)= A * x + b
-@test fixedpoint(f,[1.0; 0.1])[1] ≈ [2.0;2.5]
+@test fixedpoint(f,method =:simple_iteration,[1.0; 0.1],store_trace==true,show_trace==true,
+                 extended_trace=true)[1] ≈ [2.0;2.5]
 
 #How to call the new interface.  The above should be eventually removed
 fixedpoint(f,[1.0; 0.1])
