6a3d9f583706a72f398ba3c0fd357dcd7928388d
jasmine.yangvc@gmail.com <jasmine.yangvc@gmail.com>
2018-03-24 14:10:50 -0700

    adding new tests for anderson under fixedpoint

--- test/fixed_points.jl
+++ test/fixed_points.jl
@@ -1,6 +1,38 @@
 # Test fixed points
 @testset "fixed_points" begin
 using NLsolve, Base.Test, StaticArrays
+#TASK 1: Anderson
+
+#matrix valued function
+A = [0.5 0; 0 0.2]
+b = [1.0; 2.0]
+f(x) = A * x + b
+g(x)=f(x) - x
+@test fixedpoint(g,[1.0; 0.1], method == :anderson)
+
+#inplace matrix valued function
+A = [0.5 0; 0 0.2]
+b = [1.0; 2.0]
+function f!(x, fx)
+        fx[:] = A * x + b
+end
+g(x) = f! - x
+g(G,  x) = copy(G, g(x))
+@test fixedpoint(g,[1.0; 0.2], method == :anderson)
+
+
+# Variation using static arrays
+A= SMatrix{2,2}(0.5,0,0,0.2)
+b = SVector(1.0, 2.0)
+f(x)= A * x + b
+g(x)=f(x) - x
+@test fixedpoint(g,[1.0; 0.2],method =:anderson) # how to call new interface
+
+
+
+
+
+#TASK 2: Simple Iteration
 
 #The inplace one is not implemented, so left in the old code.
 function fixedpointOLD!(f!, x0; residualnorm = (x -> norm(x,Inf)), tol = 1E-10, maxiter=100)
@@ -39,14 +71,10 @@ b = [1.0; 2.0]
 function f!(x, fx)
         fx[:] = A * x + b
 end
-fixedpointOLD!(f!,[1.0; 0.2])
+#fixedpointOLD!(f!,[1.0; 0.2])
 
 A= SMatrix{2,2}(0.5,0,0,0.2)
 b = SVector(1.0, 2.0)
 f(x)= A * x + b
-@test fixedpoint(f,method =:simple_iteration,[1.0; 0.1],store_trace==true,show_trace==true,
+@test fixedpoint(f,[1.0; 0.2],method =:simple_iteration,[1.0; 0.1],store_trace==true,show_trace==true,
                  extended_trace=true)[1] ≈ [2.0;2.5]
-
-#How to call the new interface.  The above should be eventually removed
-fixedpoint(f,[1.0; 0.1])
-end
