b9d206078b37ca9ecf82a3cd624a2266c718d64a
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-04-19 11:05:15 +0200

    Fix anderson trace issue reported in #160. (#162)
    * Fix anderson trace issue reported in #160.
    
    * Add the problem.

--- test/iface.jl
+++ test/iface.jl
@@ -106,4 +106,23 @@ r = nlsolve(n_ary(f), [ -0.5; 1.4])
 r = nlsolve(n_ary(f), [ -0.5; 1.4], autodiff = true)
 @test converged(r)
 
+@testset "andersont trace issue #160" begin
+
+    function f_2by2!(F, x)
+        F[1] = (x[1]+3)*(x[2]^3-7)+18
+        F[2] = sin(x[2]*exp(x[1])-1)
+    end
+
+    function g_2by2!(J, x)
+        J[1, 1] = x[2]^3-7
+        J[1, 2] = 3*x[2]^2*(x[1]+3)
+        u = exp(x[1])*cos(x[2]*exp(x[1])-1)
+        J[2, 1] = x[2]*u
+        J[2, 2] = u
+    end
+
+    df = OnceDifferentiable(f_2by2!, g_2by2!, [ -0.5; 1.4], [ -0.5; 1.4])
+
+    r = nlsolve(df, [ 0.01; .99], method = :anderson, m = 10, beta=.01, show_trace=true)
+end
 end
