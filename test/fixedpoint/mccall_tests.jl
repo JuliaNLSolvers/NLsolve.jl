96da0c48804e64688e497432e74e245a54a9b477
Arnav Sood <soodarnav01@gmail.com>
2018-07-06 14:15:23 -0700

    benchmarks

--- test/fixedpoint/mccall_tests.jl
+++ test/fixedpoint/mccall_tests.jl
@@ -2,11 +2,11 @@
 using BenchmarkTools
 include("mccall_utilities.jl")
 
-# Instantiate the model. 
-mcm = McCallModel()
-
 # Benchmark the QuantEcon default solution
-@btime V, U = solve_mccall_model(mcm)
+@btime default_quantecon()
 
 # Benchmark the QuantEcon iteration scheme, with update_bellman! replaced with the state-space bellman_operator!
-@btime results = solve_mccall_model_inplace(mcm)
+@btime stacked_quantecon()
+
+# Benchmark the version using both nlsolve() and the stacked representation. 
+@btime nlsolve_quantecon()
