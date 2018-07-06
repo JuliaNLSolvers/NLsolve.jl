9a874ce4c2d054e7ff03fcf4ee3d20c312f092ed
Arnav Sood <soodarnav01@gmail.com>
2018-07-06 14:34:38 -0700

    benchmarks

--- test/fixedpoint/mccall_tests.jl
+++ test/fixedpoint/mccall_tests.jl
@@ -8,5 +8,11 @@ include("mccall_utilities.jl")
 # Benchmark the QuantEcon iteration scheme, with update_bellman! replaced with the state-space bellman_operator!
 @btime stacked_quantecon()
 
-# Benchmark the version using both nlsolve() and the stacked representation. 
-@btime nlsolve_quantecon()
+# Benchmark the version using nlsolve() with m = 0 (simple iteration). 
+@btime nlsolve_iteration()
+
+# Benchmark the version using nlsolve() and m = 2. 
+@btime nlsolve_anderson_m(2)
+
+# Benchmark the version using nlsolve() and m = 7
+@btime nlsolve_anderson_m(7)
